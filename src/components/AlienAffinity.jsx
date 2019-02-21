24d2d1c429b1b17de113bac4d712e18c50c33075
teslatickles <hunterhartline@gmail.com>
2019-02-21 12:17:37 -0600

    add two images

--- src/components/AlienAffinity.jsx
+++ src/components/AlienAffinity.jsx
@@ -2,30 +2,40 @@ import React, { Component } from "react";
 import RenderCounter from "./RenderCounter";
 import "../styles/AlienAffinity.css";
 
-let toggle;
-let i = 1;
-
 const animeImgObject = {
-    img1: "http://www.scified.com/topics/571343805484005.jpg",
+    img1:
+        "http://www.scified.com/topics/571343805484005.jpg",
     img2:
         "https://dumielauxepices.net/sites/default/files/xenomorph-clipart-alien-queen-829179-3720683.jpg",
     img3:
         "https://78.media.tumblr.com/892c9f06dacdd4c3099b8c692213cd4f/tumblr_oozvleGZR71txn1qro1_r2_1280.png",
     img4:
-        "https://i.pinimg.com/originals/6d/e1/32/6de13278901d8c4d5f426b8fa2872533.png"
+        "https://i.pinimg.com/originals/6d/e1/32/6de13278901d8c4d5f426b8fa2872533.png",
+    img5:
+        "https://cdn11.bigcommerce.com/s-0kvv9/images/stencil/1280x1280/products/200483/283385/apio6fx9f__97660.1519947716.jpg?c=2&imbypass=on",
+    img6:
+        "https://secure.i.telegraph.co.uk/multimedia/archive/03064/Alien-intro_3064438b.jpg"
 };
 
 const img = Object.values(animeImgObject);
+
+let toggle;
+let i = 0;
 
 class AlienAffinity extends Component {
     constructor(props) {
         super(props);
         this.count = 0;
-        this.state = { renderCount: 0, image: null, images: [], imgSrc: "" };
+        this.state = {
+            renderCount: 0,
+            image: null,
+            images: [],
+            imgSrc: null
+        };
     }
 
     componentWillMount() {
-        const currentImg = img[0];
+        const currentImg = img[img.length - 1];
         this.setState({
             imgSrc: currentImg
         });
@@ -43,9 +53,10 @@ class AlienAffinity extends Component {
         return;
     };
 
-    addImgToSite = img => {
-        animeImgObject.img5 = img;
-    };
+    // future feature
+    // addImgToSite = img => {
+    //     animeImgObject.img5 = img;
+    // };
 
     componentWillUnmount() {
         clearInterval(toggle);
@@ -54,10 +65,10 @@ class AlienAffinity extends Component {
     render() {
         const { imgSrc } = this.state;
         return (
-            <div>
+            <div id="main-app">
                 <>
-                    <RenderCounter renderCount={++this.count} />
                     <img id="img" src={imgSrc} alt="anime stuff" />
+                    <RenderCounter renderCount={++this.count} />
                 </>
             </div>
         );
--- src/components/RenderCounter.jsx
+++ src/components/RenderCounter.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
 import "../styles/RenderCounter.css";
 class RenderCounter extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      count: 0
-    };
-  }
+        this.state = {
+            count: 1
+        };
+    }
 
-  render() {
-    const { renderCount } = this.props;
-    return (
-      <div>
-        <h4 id="render-count">I've rendered {renderCount} times!</h4>
-      </div>
-    );
-  }
+    render() {
+        const { renderCount } = this.props;
+        return (
+            <div>
+                <h4 id="render-count">I've rendered {renderCount} times!</h4>
+            </div>
+        );
+    }
 }
 
 export default RenderCounter;
